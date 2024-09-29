241c913016f51b05abbb178c680f646d451de93d
leonardo.kleuser <leonardo.kleuser@brilliantmachine.com.br>
2024-09-29 01:42:58 -0300

    add testes unitarios

--- meu_app_api/main_test_model.py
+++ meu_app_api/main_test_model.py
@@ -8,21 +8,26 @@ from pickle import load
 np.random.seed(7)  # Definindo uma semente global
 
 # Referência ao Dataset Steam
-dataset_url = "meu_app_api\database\steam.csv"  # Altere para o caminho correto do seu dataset Steam
+dataset_url = "database/steam.csv"  # Altere para o caminho correto do seu dataset Steam
 
 # Lê o arquivo
 dataset = pd.read_csv(dataset_url, delimiter=',')
-
-# Transforma o dataset em um array
-array = dataset.values
 
 # Definir as colunas de entrada (variáveis preditoras) e a coluna alvo (price ou categorizada)
 input_columns = ['genres', 'categories']  # As colunas que serão usadas como preditores
 output_column = 'price'  # A coluna alvo (faixas de preço)
 
+# Tratamento para dados categóricos
+# Verifica se as colunas categóricas estão presentes, caso contrário, insere valores padrões
+for col in input_columns:
+    if col not in dataset.columns:
+        dataset[col] = 'None'
+
 # Transformar as colunas de entrada em variáveis dummificadas (necessário para o modelo)
 X = pd.get_dummies(dataset[input_columns], drop_first=True).values  # As variáveis preditoras
-Y = pd.cut(dataset[output_column], bins=[0, 5, 15, np.inf], labels=['Baixo', 'Médio', 'Alto']).values  # Faixas de preço como alvo
+
+# Transformar a coluna de preço em faixas categorizadas
+Y = pd.cut(dataset[output_column], bins=[0, 5, 15, np.inf], labels=['Baixo', 'Médio', 'Alto']).astype(str).values  # Faixas de preço como alvo
 
 # Carregar o modelo salvo
 ml_path = 'ml_model/steam_model.pkl'  # Caminho do modelo salvo (alterar conforme necessário)
@@ -70,8 +75,8 @@ class TestModel:
         return precision
 
     @staticmethod
-    def f1_score() -> float:
-        """Verifica o valor de "f1_score" do modelo."""
+    def get_f1_score() -> float:
+        """Verifica o valor de 'f1_score' do modelo."""
         f1 = f1_score(Y, prediction, average='macro')
         return f1
 
@@ -83,4 +88,4 @@ print(f"Percentual de jogos na faixa 'Alto': {Dataset.getPop() * 100:.2f}%")
 print(f"Acurácia do modelo: {TestModel.get_acc():.3f}")
 print(f"Recall do modelo: {TestModel.get_recall():.3f}")
 print(f"Precisão do modelo: {TestModel.get_precision():.3f}")
-print(f"F1 Score do modelo: {TestModel.f1_score():.3f}")
+print(f"F1 Score do modelo: {TestModel.get_f1_score():.3f}")
